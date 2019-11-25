7feb93651238b6f0a8d081845ea4f185417595d0
piotrek <piotrek@DESKTOP-IQBRRVD>
2019-11-25 23:02:02 +0100

    add file service (create new file and read from file methods)

---
+++ PersonalDataXmlStorageApp/Services/FileService.cs
@@ -0,0 +1,55 @@
+﻿using PersonalDataXmlStorageApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PersonalDataXmlStorageApp.Services
+{
+    public class FileService
+    {
+        private const string _fileName = @"PersonalData.xml";
+
+        private void CreateNewFile()
+        {
+            try
+            {
+                using (var writer = new StreamWriter($@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\{_fileName}"))
+                {
+                    var serializer = new XmlSerializer(typeof(List<Person>));
+                    serializer.Serialize(writer, new List<Person>());
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        internal List<Person> GetPersonsFromFile()
+        {
+            try
+            {
+                using (var stream = new StreamReader($@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\{_fileName}"))
+                {
+                    var serializer = new XmlSerializer(typeof(List<Person>));
+                    var persons = (List<Person>)serializer.Deserialize(stream);
+
+                    return persons;
+                }
+            }
+
+            catch (FileNotFoundException)
+            {
+                CreateNewFile();
+                return new List<Person>();
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
