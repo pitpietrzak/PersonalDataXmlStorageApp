ade0195fd65a35d963d6be80eb5546dda3584fb4
piotrek <piotrek@DESKTOP-IQBRRVD>
2019-11-25 22:37:14 +0100

    add person model

---
+++ PersonalDataXmlStorageApp/Models/Person.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace PersonalDataXmlStorageApp.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _lastName;
+    
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        private string _streetName;
+
+        public string StreetName
+        {
+            get => _streetName;
+            set
+            {
+                _streetName = value;
+                OnPropertyChanged(nameof(StreetName));
+            }
+        }
+
+        private string _houseNumber;
+
+        public string HouseNumber
+        {
+            get => _houseNumber;
+            set
+            {
+                _houseNumber = value;
+                OnPropertyChanged(nameof(HouseNumber));
+            }
+        }
+
+        private string _apartmentNumber;
+
+        public string ApartmentNumber
+        {
+            get => _apartmentNumber;
+            set
+            {
+                _apartmentNumber = value;
+                OnPropertyChanged(nameof(ApartmentNumber));
+            }
+        }
+
+        private string _postalCode;
+
+        public string PostalCode
+        {
+            get => _postalCode;
+            set
+            {
+                _postalCode = value;
+                OnPropertyChanged(nameof(PostalCode));
+            }
+        }
+
+        private string _town;
+
+        public string Town
+        {
+            get => _town;
+            set
+            {
+                _town = value;
+                OnPropertyChanged(nameof(Town));
+            }
+        }
+
+        private string _phoneNumber;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        private DateTime _birthDate;
+
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged(nameof(BirthDate));
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - BirthDate.Year;
+                if (now < BirthDate.AddYears(age)) age--;
+
+                return age;
+            }
+        }
+
+        [XmlIgnore]
+        public bool IsDirty { get; set; }
+
+        [XmlIgnore]
+        public bool IsNew { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(propertyName, true);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName, bool makeDirty)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            if (makeDirty)
+                IsDirty = true;
+        }
+    }
+}
